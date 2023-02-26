f9c1654a6b7045bb7d181ea1fb2344009b0b35b2
Ewelina <jamrozik.ewelina@gmail.com>
2023-02-26 13:04:03 +0100

    Update idexes saving

--- src/preprocess.py
+++ src/preprocess.py
@@ -28,9 +28,19 @@ def save_idxs_for_processed(idxs, dataset, data_type):
     Returns:
         None
     """
+    '''
     idxs_dict = {
         dataset: idxs.tolist()
         }
     path = '{}_idxs.txt'.format(data_type)
     with open(path, 'w') as json_file:
         json.dump(idxs_dict, json_file)
+    '''
+
+    path = '{}_idxs.txt'.format(data_type)
+    with open(path, 'r+') as json_file:
+        json_data = json.load(json_file)
+        json_data[dataset] = idxs.tolist()
+        json_file.seek(0)
+        json.dump(json_data, json_file)
+        json_file.truncate()
